e9976fe1da185e711ca7d4b9bbb42ef63086da50
Ethan Lawrence <28376972+EthanLawr@users.noreply.github.com>
2023-09-14 12:03:23 -0400

    Add files via upload

---
+++ eclipse-workspace/Lab2/src/ArrayDemo6.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList; // Needed for ArrayList class
+
+public class ArrayDemo6 {
+
+	   public static void main(String[] args)
+	   {
+	      // Create an ArrayList to hold BankAccount objects.
+	      ArrayList<BankAccount> list = new ArrayList<BankAccount>();
+	      
+	      // Add three BankAccount objects to the ArrayList.
+	      list.add(new BankAccount(100.0));
+	      list.add(new BankAccount(500.0));
+	      list.add(new BankAccount(1500.0));
+	      
+	      // Display each item.
+	      for (int index = 0; index < list.size(); index++)
+	      {
+	         BankAccount account = list.get(index);
+	         System.out.println("Account at index " + index +
+	                      "\nBalance: " + account.getBalance());
+	      }      
+	   }
+
+}
+
+class BankAccount {
+}
