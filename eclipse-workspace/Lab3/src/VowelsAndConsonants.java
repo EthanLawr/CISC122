9943d3775baa91dfa1bbf398cbe52b3995dab8ac
Ethan Lawrence <28376972+EthanLawr@users.noreply.github.com>
2023-09-28 13:28:05 -0400

    Update VowelsAndConsonants.java

--- eclipse-workspace/Lab3/src/VowelsAndConsonants.java
+++ eclipse-workspace/Lab3/src/VowelsAndConsonants.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;  // Import the Scanner class
+import java.util.ArrayList; // Import the ArrayList class
+import java.util.Arrays;
+import java.util.regex.Pattern;
 public class VowelsAndConsonants {
-	
+	private static String chosenWord = "";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Create a Scanner object
+		// Create a Scanner object for the initial input string and obtain the inital input
+		Scanner initialWord = new Scanner(System.in);
+		System.out.print("Please input a string to continue this program:\t");
+		chosenWord = initialWord.nextLine();
+		
+		// Create a Scanner object for the initial choice input and obtain the initial choice input
 	    Scanner option = new Scanner(System.in);  
 	    int check = 0;
 	    System.out.print("Please choose one of the following options:\n" +
@@ -16,27 +24,33 @@ public class VowelsAndConsonants {
 		    // Switch with toLowerCase to accept user input options.
 		    switch (chosenOption.toLowerCase()) {
 		    case("a"): 
-		    	Scanner word = new Scanner(System.in);
-		    	// Some Additional User Input
-		    	System.out.println("Please input your word for option A:\t");
-		    	String chosenWord = word.nextLine();
-		    	System.out.println("There are " + Lotty.vowelCount(chosenWord) + " vowel(s)" +
-		    	 " in your word.");
-		    	
+		    	// Use the input string on Lotty
+		    	int vowelCount = Lotty.vowelCount(chosenWord);
+	    		// If statement for grammar. Create desired output.
+		    	if (vowelCount != 1) System.out.println("There are " + vowelCount + " vowels in your word.");
+		    	else System.out.println("There is " + vowelCount + " vowel in your word.");
 		    	break;
 		    case("b"):
-		    	// Some Additional User Input
-		    	System.out.println("something");
+		    	// Use the input string on Lotty
+		    	int consonantCount = Lotty.consonantCount(chosenWord);
+		    	// If statement for grammar. Create desired output.
+		    	if (consonantCount != 1) System.out.println("There are " + consonantCount + " consonants in your word.");
+		    	else System.out.println("There is " + consonantCount + " consonant in your word.");
 		    	break;
 		    case("c"):
-		    	// Some Additional User Input
-		    	System.out.println("something");
+		    	// Obtain a new input
+		    	System.out.print("Please input your string:\t");
+		    	Scanner word = new Scanner(System.in);
+		    	// Insert input into private variable
+	    	    chosenWord = word.nextLine();
+	    	    System.out.println("Your word is now \"" + chosenWord + "\".");
 		    	break;
 		    case("d"):
 		    	// Breaks the loop.
 		    	check += 1;
 		    	break;
 		    default:
+		    	// In the case an option is not chosen the question is reiterated.
 			    System.out.print("Please choose a valid one of the following options:\n" +
 			    		"A.Count the number of vowels in the string;\n" +
 			    		"B.Count the number of consonants in the string.\n" +
@@ -54,15 +68,45 @@ public class VowelsAndConsonants {
 }
 
 class Lotty {
+	// Arraylist used for the "contains" method. Initialized with vowels.
+	private static ArrayList<String> vowels = new ArrayList<String>() {
+	    {
+	        add("a"); add("e"); add("i"); add("o"); add("u");
+	    }
+	};
+	// Required so that whitespaces and symbols are not counted
+	private static ArrayList<String> consonants = new ArrayList<String>() {
+	    {
+	    	add("b"); add("c"); add("d"); add("f"); add("g");
+	    	add("h"); add("j"); add("k"); add("l"); add("m");
+	    	add("n"); add("p"); add("q"); add("r"); add("s");
+	    	add("t"); add("v"); add("w"); add("x"); add("y");
+	    	add("z");
+	    }
+	};
+	
 	Lotty(String x) {
-		
+		// What would we need to do here?
+		// Is this where he wants me to accept a string value with Scanner?
+		// If so, I still dont see why Option C is required as both A and B can accept a new string. C only accepts an input while 
 	}
 	
 	static int vowelCount(String w) {
-		String[] array = w.split(",");
-		return 1;
+		String[] word = Pattern.compile("^[a-zA-Z]+?$").split(w);
+		System.out.println(Arrays.toString(word));
+		int vowelCount = 0;
+		for (int i = 0; i < word.length; i++) {
+			if (vowels.contains(word[i])) vowelCount += 1;
+		}
+		return vowelCount;
 	}
-	int consonantCount(String w) {
-		return 1;
+	// Opposite of vowel count
+	static int consonantCount(String w) {
+		char[] word = w.toCharArray();
+		int consonantCount = word.length;
+		for (int i = 0; i < word.length; i++) {
+			if (consonants.contains(word[i])) consonantCount += 1;
+		}
+		return consonantCount;
 	}
 }
