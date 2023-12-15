99ec4d63b01160a885bf13064057912316f62a6b
Ethan Lawrence <elawrence152@gmail.com>
2023-12-14 23:25:43 -0500

    Class competion.
    101% grade.

--- eclipse-workspace/CSS122FinalProject/CSS122FinalProject/src/LightsOutColor.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Color;
-
-// Color class in-case additional colors were needed or custom colors
-public class LightsOutColor {
-	public static final Color BLACK = Color.BLACK;
-	public static final Color WHITE = Color.WHITE;
-	public static final Color GRAY = Color.GRAY;
-}
